ca4fd8f0dced47bcafdc7e8a39e3701d760dcf2a
Petter Carlsson <petter0619@gmail.com>
2023-01-13 08:09:42 +0100

    Comments added to server setup

--- src/server.js
+++ src/server.js
@@ -8,9 +8,13 @@ const { startStandaloneServer } = require('@apollo/server/standalone')
 
 async function run() {
 	try {
+		// Loads our schema.graphql file and reformats it for use in the next step
 		const typeDefs = await loadFiles(path.join(__dirname, 'schema.graphql'))
+		// Creates a schema from our typeDefs (see step above) and our resolvers
 		const schema = makeExecutableSchema({ typeDefs: typeDefs, resolvers: resolvers })
+		// Creates a GraphQL server from our schema
 		const server = new ApolloServer({ schema: schema })
+		// Starts the server in 
 		const res = await startStandaloneServer(server)
 		console.log(`🚀 Server ready at ${res.url}`)
 	} catch (error) {
